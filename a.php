16dbf4a5a0eb14172e2713ac806b10230f3b3154
i085ono <138636970+i085ono@users.noreply.github.com>
2023-07-13 15:12:29 +0900

    Update a.php

--- a.php
+++ a.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
 <?php 
-  foreach(file('/usr/share/dict/words') as $line){
+  foreach(file('/usr/share/dict/words') as $line){  //"linux.words"の"linux."の部分が不要だから。
     // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
     if(strpos($line,"abc")     ) 
   } 
